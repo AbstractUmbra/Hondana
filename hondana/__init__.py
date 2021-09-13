b8ada30d268bde94ac7ffdddc8567638ab2ac090
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2021-09-13 15:50:01 +0100

    version bump, woops

--- hondana/__init__.py
+++ hondana/__init__.py
@@ -26,7 +26,7 @@ __title__ = "Hondana"
 __author__ = "AbstractUmbra"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present AbstractUmbra"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 import logging
 from typing import Literal, NamedTuple
@@ -57,6 +57,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=2, micro=2, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=0, minor=2, micro=3, releaselevel="final", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
