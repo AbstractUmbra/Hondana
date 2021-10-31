80b0c17c5b4af3379b3684078d209d0b808700ee
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2021-10-31 03:05:39 +0000

    add comments to example

--- examples/download_manga.py
+++ examples/download_manga.py
@@ -10,10 +10,15 @@ client = hondana.Client(username="my-username", password="my-password")
 
 
 async def main():
+    # Get the manga, we will need it's chapters
     manga = await client.view_manga("some-manga-id-here")
 
+    # Load the feed of the manga, that contains all chapters.
+    # To note... I would filter by language here, else you'll potentially have random translations downloaded.
     chapters = await manga.feed(limit=500, offset=0, translated_language=["en"])
 
+    # This is how you recursively download the chapters.
+    # The string in the `.download()` call is the path to save all the chapters in. It will recursively create it, if needed.
     for chapter in chapters:
         await chapter.download(f"{manga.title}/{chapter.chapter}")
 
