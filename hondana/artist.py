404fe60ed6f225f31b955164844147ca35f11deb
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2021-11-24 19:55:51 +0000

    artist updates

--- hondana/artist.py
+++ hondana/artist.py
@@ -30,9 +30,12 @@ from .utils import require_authentication
 
 
 if TYPE_CHECKING:
+    from .manga import Manga
     from .http import HTTPClient
     from .types.artist import ArtistResponse
     from .types.common import LocalisedString
+    from .types.manga import MangaResponse
+    from .types.relationship import RelationshipResponse
 
 
 __all__ = ("Artist",)
@@ -97,13 +100,14 @@ class Artist:
         "_created_at",
         "_updated_at",
         "version",
+        "__manga",
     )
 
     def __init__(self, http: HTTPClient, payload: ArtistResponse) -> None:
         self._http = http
         self._data = payload
         self._attributes = self._data["attributes"]
-        self._relationships = self._data["relationships"]
+        self._relationships: list[RelationshipResponse] = self._data["relationships"]
         self.id: str = self._data["id"]
         self.name: str = self._attributes["name"]
         self.image_url: Optional[str] = self._attributes["imageUrl"]
@@ -121,6 +125,7 @@ class Artist:
         self.version: int = self._attributes["version"]
         self._created_at = self._attributes["createdAt"]
         self._updated_at = self._attributes["updatedAt"]
+        self.__manga: Optional[list[Manga]] = None
 
     def __repr__(self) -> str:
         return f"<Artist id={self.id} name='{self.name}'>"
@@ -137,6 +142,75 @@ class Artist:
     def updated_at(self) -> datetime.datetime:
         """When this artist was last updated."""
         return datetime.datetime.fromisoformat(self._updated_at)
+
+    @property
+    def manga(self) -> Optional[list[Manga]]:
+        if self.__manga is not None:
+            return self.__manga
+
+        if not self._relationships:
+            return None
+
+        manga_: list[MangaResponse] = []
+        for item in self._relationships:
+            if item["type"] == "manga":
+                manga_.append(item)
+
+        if not manga_:
+            return None
+
+        formatted: list[Manga] = []
+        from .manga import Manga
+
+        for item in manga_:
+            if "attributes" in item:
+                formatted.append(Manga(self._http, item))
+
+        if not formatted:
+            return
+
+        self.__manga = formatted
+        return self.__manga
+
+    @manga.setter
+    def manga(self, value: list[Manga]) -> None:
+        fmt = []
+        for item in value:
+            if isinstance(item, Manga):
+                fmt.append(item)
+
+        self.__manga = fmt
+
+    async def get_manga(self) -> Optional[list[Manga]]:
+        if self.manga is not None:
+            return self.manga
+
+        if not self._relationships:
+            return
+
+        manga_relationships: list[MangaResponse] = []
+        for item in self._relationships:
+            if item["type"] == "manga":
+                manga_relationships.append(item)
+
+        if not manga_relationships:
+            return
+
+        formatted: list[Manga] = []
+        from .manga import Manga  # TODO: Fix circular.
+
+        for manga in manga_relationships:
+            if "attributes" in manga:
+                formatted.append(Manga(self._http, manga))
+            else:
+                data = await self._http._view_manga(manga["id"], includes=["author", "artist", "cover_art", "manga"])
+                formatted.append(Manga(self._http, data["data"]))
+
+        if not formatted:
+            return
+
+        self.__manga = formatted
+        return self.__manga
 
     @require_authentication
     async def update(self, *, name: Optional[str] = None, version: int) -> Artist:
