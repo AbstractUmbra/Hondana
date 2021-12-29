48f5042460ad49efb6a89cc2ca79990a2cb146c5
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2021-12-29 21:02:01 +0000

    version bump to alpha for testing

--- hondana/__init__.py
+++ hondana/__init__.py
@@ -60,6 +60,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=1, minor=1, micro=5, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=1, minor=1, micro=6, releaselevel="alpha", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
