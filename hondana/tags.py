1455c31efe3abcf9896be9acfbaed3f0b0c076af
Alex Nørgaard <umbra@abstractumbra.dev>
2023-12-19 20:52:46 +0000

    add missing type: ignore due to formatting

--- hondana/tags.py
+++ hondana/tags.py
@@ -81,7 +81,8 @@ class Tag:
         self._data = payload
         self._attributes = payload["attributes"]
         self._relationships: list[RelationshipResponse] = self._data.pop(  # type: ignore # can't pop from a TypedDict
-            "relationships", []  # type: ignore # can't pop from a TypedDict
+            "relationships",  # type: ignore # can't pop from a TypedDict
+            [],  # type: ignore # can't pop from a TypedDict
         )  # TODO: remove this when they have relationships to be in line.
         self._name = self._attributes["name"]
         self.id: str = payload["id"]
