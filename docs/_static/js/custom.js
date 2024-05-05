763b36cac39047ca1855ddc850af4da6c7f72724
Alex Nørgaard <umbra@abstractumbra.dev>
2024-05-05 15:47:31 +0100

    fix documentation issues

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,14 +1,14 @@
 const classes = document.getElementsByClassName("class");
-const tables =  document.getElementsByClassName('py-attribute-table');
+const tables = document.getElementsByClassName('py-attribute-table');
 
 for (let i = 0; i < classes.length; i++) {
-   const parentSig = classes[i].getElementsByTagName('dt')[0];
-   const table = tables[i];
+    const parentSig = classes[i].getElementsByTagName('dt')[0];
+    const table = tables[i];
 
-   parentSig.classList.add(`parent-sig-${i}`);
-   table.id = `attributable-${i}`
+    parentSig.classList.add(`parent-sig-${i}`);
+    table.id = `attributable-${i}`
 
-   $(`#attributable-${i}`).insertAfter( `.parent-sig-${i}` );
+    $(`#attributable-${i}`).insertAfter(`.parent-sig-${i}`);
 }
 
 
